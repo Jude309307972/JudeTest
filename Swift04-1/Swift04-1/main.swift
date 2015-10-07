4cdf0039e17e62afcfebf4459a87771abd6e4d90
Jude <309307972@qq.com>
2015-10-07 15:54:50 +0800

    06

--- Swift04-1/Swift04-1/main.swift
+++ Swift04-1/Swift04-1/main.swift
@@ -8,57 +8,35 @@
 
 import Foundation
 
-func add(var a:Int, b:Int) -> Int{
-    a = 1
-    return a + b
+var names = ["Swift","Arial","Soga","Donary"]
+
+
+// 一 使用函数
+func backwards(firstString:String, secondString: String) -> Bool {
+    return firstString > secondString
 }
 
-func add(a:Int, b:Int, c: Int) -> Int {
-    return a + b + c
+var reversed = names.sort(backwards)
+print(reversed)
+
+reversed = names.sort({
+    (fir: String, sec: String) ->Bool in return fir < sec
+})
+print(reversed)
+
+reversed = names.sort({
+    (fir, sec) -> Bool in fir > sec
+})
+print(reversed)
+
+reversed = names.sort({$0 > $1})
+
+print(reversed)
+
+let strings = names.map{
+    (str: String) -> String in
+    return str + "hehe"
 }
-
-func printString(strings: String...){
-    for string  in strings{
-        print(string)
-    }
-}
-
-func add(addFun:(Int,Int,Int) -> Int, a:Int, b:Int) {
-    print(addFun(a,b,50))
-}
-
-func minus(a: Int,b:Int) ->Int {
-    return a - b
-}
-
-enum FunctionType {
-    case Add
-    case Minus
-}
-
-func chooseFuntion(funType: FunctionType) ->(Int,Int) ->Int {
-    return funType == FunctionType.Add ? add: minus
-}
-
-func mainFunction(input:Int) ->Int {
-    func internalFucntion(input: Int) ->Int {
-        return input - 1
-    }
-    return internalFucntion(input - 1)
-}
-
-var a = 10, b = 20
-
-print(add(10, b: 20))
-
-print(add(a, b: b , c: 10))
-
-printString("haha", "hehe")
-printString()
-
-add(add, a: a , b: b)
-
-let fun = chooseFuntion(FunctionType.Minus)
-print(fun(a,b))
+print(strings)
 
 
